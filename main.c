1d7227fa19031dec7899190ac90a539830a26099
wshuang <wshuang808@gmail.com>
2015-04-10 09:19:30 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main() {
+    unsigned int numCity = 0;
+    printf("Input number of city:");
+    scanf("%d", &numCity);
+    printf("%d cities in total.\n", numCity);
+
+    int adjacencyMatrix[numCity][numCity];
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+main: main.c
+	gcc -o bin/im main.c
