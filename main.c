5b476268dd4af04eec3ee61e5555bb9897c66622
wshuang <wshuang808@gmail.com>
2015-04-11 23:05:19 +0800

    First draft with a major problem.

--- main.c
+++ main.c
@@ -1,66 +1,149 @@
 # include <stdio.h>
 # include <stdbool.h>
-# include <string.h>
+# include <limits.h>
+
 
 # define NUM_MAX_CITY 10
-# define NO_ROUTE -1
+# define TIME_INF INT_MAX
+# define TIME_IN_SAME_CITY 0
+# define NULL_CITY -1
+# define VISITED 1
+# define UNVISITED 0
 
-bool getDataFromPlayer(unsigned int*, int[NUM_MAX_CITY][NUM_MAX_CITY]);
-void normalizeData(int[NUM_MAX_CITY][NUM_MAX_CITY]); 
-void traceInput(unsigned int, int[NUM_MAX_CITY][NUM_MAX_CITY]);
 
-int main() {
-    unsigned int numCity = 0;
-    int ajMatrix[NUM_MAX_CITY][NUM_MAX_CITY];
-    memset(ajMatrix , NO_ROUTE, sizeof(ajMatrix[0][0]) * NUM_MAX_CITY * NUM_MAX_CITY);
+bool getDataFromPlayer();
+void normalizeData(); 
+void traceInput();
+void caculation();
+void initIntArrayByValue(int* pArray, unsigned int length, int value);
+void traverseCity(unsigned int cityId); 
+void updateCityData(unsigned int cityId, unsigned int preCityId, int minTime);
+void evaluatePath(unsigned int startCity, unsigned int endCity);
 
-    bool dataReceived = getDataFromPlayer(&numCity, ajMatrix);
-    if (dataReceived) {
-        normalizeData(ajMatrix);
-        traceInput(numCity, ajMatrix);
+
+unsigned int numCity;
+int matrix[NUM_MAX_CITY][NUM_MAX_CITY];
+int minTimes[NUM_MAX_CITY];
+int preCities[NUM_MAX_CITY];  // previous city on the shortest path for each city
+
+
+int main(){
+    bool dataReceived = getDataFromPlayer();
+    if (dataReceived){
+        normalizeData();
+        traceInput();
     }
 }
 
 
-bool getDataFromPlayer(unsigned int* numCity, int matrix[NUM_MAX_CITY][NUM_MAX_CITY]) {
+bool getDataFromPlayer(){
     bool result = false;
     printf("Input number of city (1-100):");
-    scanf("%d", numCity);
-    if (1 <= *numCity && *numCity <= NUM_MAX_CITY) {
-        for (int i = 1; i < *numCity; ++i) {
-            for (int j = 0; j < i; ++j) {
+    scanf("%d", &numCity);
+    if (1 <= numCity && numCity <= NUM_MAX_CITY){
+        for (unsigned int i = 1; i < numCity; ++i){
+            for (unsigned int j = 0; j < i; ++j){
                 scanf("%d", &matrix[i][j]);
             }
         }
         result = true;
     }
     else {
-        printf("Invalid city number %d.\n", *numCity);
+        printf("Invalid city number %d.\n", numCity);
     }
     return result;
 }
 
 
-void normalizeData(int matrix[NUM_MAX_CITY][NUM_MAX_CITY]) {
-    for (int i = 0; i < NUM_MAX_CITY; ++i) {
-        for (int j = 0; j < NUM_MAX_CITY; ++j) {
-            if (i == j) {
-                matrix[i][j] = 0;
+void normalizeData(){
+    for (unsigned int i = 0; i < numCity; ++i){
+        for (unsigned int j = 0; j < i + 1; ++j){
+            if (i == j){
+                matrix[i][j] = TIME_IN_SAME_CITY;
             }          
-            else if (i < j) {
-                matrix[i][j] = matrix[j][i];
+            else if (i > j){
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = UNVISITED;
             }
         }
     }
 }
 
 
-void traceInput(unsigned int numCity, int ajMatrix[NUM_MAX_CITY][NUM_MAX_CITY]) {
+void traceInput(){
     printf("%d cities in total.\n", numCity);
-    for (int i = 0; i < NUM_MAX_CITY; ++i) {
-        for (int j = 0; j < NUM_MAX_CITY; ++j) {
-            printf("%d ", ajMatrix[i][j]);
+    for (unsigned int i = 0; i < numCity; ++i){
+        for (unsigned int j = 0; j < numCity; ++j){
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 }
+
+
+void caculation(){
+    initIntArrayByValue(minTimes, numCity, TIME_INF);
+    minTimes[0] = TIME_IN_SAME_CITY; // set time for capitol
+    initIntArrayByValue(preCities, numCity, NULL_CITY);
+
+    traverseCity(0);
+} 
+
+
+void traverseCity(unsigned int rootCity){
+    unsigned int startCity, endCity;
+    startCity = rootCity;
+    endCity = startCity + 1;
+
+    int pathTime, minTime;
+    for (; endCity < numCity; ++endCity){
+        if (matrix[endCity][startCity] == UNVISITED){
+            matrix[endCity][startCity] = VISITED; // set path visited
+            evaluatePath(startCity, endCity);
+            traverseCity(endCity);
+        }
+    }
+}
+
+
+void evaluatePath(unsigned int startCity, unsigned int endCity){
+    int pathTime = matrix[startCity][endCity];
+    int minTime;
+    if (pathTime != TIME_INF){
+        if (minTimes[endCity] == TIME_INF){
+            minTime = minTimes[startCity] + pathTime;
+            updateCityData(endCity, startCity, minTime);
+        }
+        else{
+            minTime = minTimes[startCity] + pathTime;
+            if (minTimes[endCity] > minTime){
+                updateCityData(endCity, startCity, minTime);
+            }
+            else{
+                minTime= minTimes[endCity] + pathTime;
+               if (minTimes[startCity] > minTime){
+                   updateCityData(startCity, endCity, minTime);
+               } 
+            }
+        }
+    }
+}
+
+
+void updateCityData(unsigned int cityId, unsigned int preCityId, int minTime){
+    unsigned int oldPreCity = preCities[cityId];
+    minTimes[cityId] = minTime;
+    preCities[cityId] = preCityId;
+
+    if (oldPreCity != NULL_CITY){
+        evaluatePath(cityId, preCityId);
+    }
+}
+
+
+void initIntArrayByValue(int* pArray, unsigned int length, int value){
+    for (unsigned int i = 0; i < length; ++i){
+        pArray[i] = value;
+    }
+}
+
